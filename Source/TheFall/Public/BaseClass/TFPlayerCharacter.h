d16e96021f08416240d2ce7f944a69d9daff11aa
Cody <42767450+SkyZerFoxs@users.noreply.github.com>
2025-10-21 22:15:35 +0200

    Commit + Add Statline component + Add movement player

---
+++ Source/TheFall/Public/BaseClass/TFPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseClass/TFCharacter.h"
+#include "TFPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEFALL_API ATFPlayerCharacter : public ATFCharacter
+{
+	GENERATED_BODY()
+	
+};
